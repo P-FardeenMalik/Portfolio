cdd17ebd39308c768bdc30d0750386a7d3c23116
P-FardeenMalik <fardeenmalik138@gmail.com>
2024-07-30 20:44:54 +0530

    Updated

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -256,7 +256,7 @@ const Hero = () => {
             <HeroRightContainer>
               <motion.div {...headContentAnimation}>
                 <Tilt>
-                  <Img src={HeroImg} alt="Rishav Chanda" />
+                  <Img src={HeroImg} alt="Fardeen Maik" />
                 </Tilt>
               </motion.div>
             </HeroRightContainer>
