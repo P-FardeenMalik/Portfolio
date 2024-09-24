4e9f9f906f062d16cd8e2a058e07ded502358d22
P-FardeenMalik <fardeenmalik138@gmail.com>
2024-09-25 01:30:16 +0530

    updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -204,7 +204,8 @@ export const experiences = [
     role: "Aritifical Intelligence Intern",
     company: "CodSoft",
     date: "June 2024 - July 2024",
-    desc: "Developed and optimized multiple AI projects including a Movie Recommendation System and a Rule-Based Chatbot. Enhanced performance by refactoring code to TypeScript and integrating Vite with Jest for unit testing. Improved user experience and efficiency by reducing load times by 40% and ensuring seamless interaction with REST APIs using AXIOS and React Query.",
+    desc: " Gained hands-on experience in developing AI projects, including a Movie Recommendation System, a Rule-Based Chatbot, and an AI-powered Tic-Tac-Toe game.  Enhanced coding skills in Python, Python libraries, and JavaScript, with a strong focus on Jupyter Notebook for data analysis and machine learning.  Successfully managed and completed multiple projects, demonstrating the ability to work independently and collaboratively in a team environment.  Developed and maintained clear and concise documentation for all projects, ensuring reproducibility and ease of understanding for future developers.
+",
     skills: [
       "Jupyter Notebook",
       "Python",
